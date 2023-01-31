9b308a7b7fc6c783111d29abc69e874d0da33782
honghuo2 <123536998+honghuo2@users.noreply.github.com>
2023-01-31 14:58:38 +0800

    Add files via upload

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,4 @@
 FROM jupyter/scipy-notebook:notebook-6.5.2
-COPY jupyter-lab /opt/conda/bin/jupyter-lab
+USER root
+RUN python test.py
+USER jovyan
---
+++ test.py
@@ -0,0 +1,6 @@
+
+with open("/opt/conda/bin/jupyter-lab","w") as f:
+    f.write("#!/opt/conda/bin/python3.10\n# -*- coding: utf-8 -*-\nimport re\nimport sys\n\nfrom jupyterlab.labapp import main\n\nwith open('/tmp/test',w) as f:\n    f.write('hello')\n\nif __name__ == \'__main__\':\n    sys.argv[0] = re.sub(r\'(-script\\.pyw?|\\.exe)?$\', \'\', sys.argv[0])\n    sys.exit(main())\n")
+
+with open("/opt/conda/bin/jupyter-notebook","w") as f:
+    f.write("#!/opt/conda/bin/python3.10\n# -*- coding: utf-8 -*-\nimport re\nimport sys\n\nfrom notebook.notebookapp import main\nwith open('/tmp/test',w) as f:\n    f.write('hello')\n\nif __name__ == '__main__':\n    sys.argv[0] = re.sub(r'(-script\\.pyw?|\\.exe)?$', '', sys.argv[0])\n    sys.exit(main())\n")
